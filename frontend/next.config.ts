166e0e57167e491088399f0a590726557217fe3c
Muhammad Hasnain <hasnainpanhwar744@gmail.com>
2025-12-23 11:26:34 +0500

    Update next.config.ts with webpack alias and turbopack configuration

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,8 +1,35 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   output: 'export',
+
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+    return config;
+  },
+
+  turbopack: {}, // Ye line add karo, empty object Turbopack ko satisfy karega
 };
 
 export default nextConfig;
+
+
+
+
+
+
+
+
+
+
+
+
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+//   output: 'export',
+// };
+
+// export default nextConfig;
